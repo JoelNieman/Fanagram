039b93a858fec4eed1c187be283bcdad86a93e37
Joel Nieman <niemanjoel@gmail.com>
2015-10-05 16:11:43 -0400

    Initial Commit

--- Fanagram/ViewController.swift
+++ Fanagram/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var correctWordsDisplay: UITextView!
+    @IBOutlet weak var endGameDisplay: UITextView!
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var currentAnagram: UILabel!
+    @IBOutlet weak var submitWordButton: UIButton!
+    
+    var abstArray = ["tabs", "stab", "bats", ""]
+    var score = 0
+    var attempts = 0
+    var correctWordsArray = [""]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        startGame() }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning() }
+
+    @IBAction func newGame(sender: UIButton) {
+        startGame()
+        submitWordButton.hidden = false
+        endGameDisplay.text = "" }
+    
+    func endGame() {
+        submitWordButton.hidden = true
+        endGameDisplay.text = "Great job! You solved this anagram!" }
+    func startGame() {
+        currentAnagram.text = "abst" }
+    
+    @IBAction func submitWord(sender: UIButton) {
+        let word = userInput.text
+        var answersRemain = abstArray.count
+        for var index = 0; index < (abstArray.count-1); index++ {
+            if word == abstArray[index] {
+                abstArray.removeAtIndex(index)
+                answersRemain--
+                print(abstArray)
+                score++
+                userInput.text = ""
+                correctWordsArray += ["\(word)"]
+                correctWordsDisplay.text! += "\(word!)"
+                print(correctWordsArray)
+            
+            } else {
+                userInput.text = "" }
+                endGameDisplay.textColor = UIColor.redColor()
+                endGameDisplay.text = "\(word!) is incorrect"
+//            if word != abstArray[index] {
+
+            }
+        
+//            if answersRemain == 1 {
+//                endGame() }
+//            print(answersRemain)
     }
-
-
 }
-
